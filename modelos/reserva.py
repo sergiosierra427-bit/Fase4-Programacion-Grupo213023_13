561bb07ceebd3c0b42f95685160bfc633271baaa
sergiosierra427-bit <sergiosierra427@gmail.com>
2026-07-21 16:21:14 -0500

    Update reserva.py

--- modelos/reserva.py
+++ modelos/reserva.py
@@ -1,1 +1,20 @@
-# Clase Reserva
+"""
+Clase Reserva
+"""
+
+from datetime import datetime
+
+
+class Reserva:
+
+    def __init__(self, cliente, servicio, fecha):
+        self.cliente = cliente
+        self.servicio = servicio
+        self.fecha = fecha
+
+    def mostrar_reserva(self):
+        return (
+            f"\nCliente: {self.cliente.get_nombre()}"
+            f"\nServicio: {self.servicio.mostrar_detalle()}"
+            f"\nFecha: {self.fecha.strftime('%d/%m/%Y %H:%M')}"
+        )
