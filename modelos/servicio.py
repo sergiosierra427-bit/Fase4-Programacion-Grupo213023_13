43c205d3f8f8c47b80537f58f987b6c019d79299
sergiosierra427-bit <sergiosierra427@gmail.com>
2026-07-21 17:21:09 -0500

    Update servicio.py

--- modelos/servicio.py
+++ modelos/servicio.py
@@ -12,32 +12,54 @@ class Servicio(ABC):
         self.precio = precio
 
     @abstractmethod
+    def calcular_costo(self, impuesto=0, descuento=0):
+        pass
+
+    @abstractmethod
     def mostrar_detalle(self):
         pass
 
 
-class CorteCabello(Servicio):
+class ReservaSala(Servicio):
 
     def __init__(self):
-        super().__init__("Corte de Cabello", 30000)
+        super().__init__("Reserva de Sala", 50000)
+
+    def calcular_costo(self, impuesto=0, descuento=0):
+        total = self.precio
+        total += total * impuesto
+        total -= total * descuento
+        return total
 
     def mostrar_detalle(self):
-        return f"{self.nombre} - ${self.precio}"
+        return f"{self.nombre} - ${self.calcular_costo():,.0f}"
 
 
-class Manicure(Servicio):
+class AlquilerEquipo(Servicio):
 
     def __init__(self):
-        super().__init__("Manicure", 45000)
+        super().__init__("Alquiler de Equipo", 80000)
+
+    def calcular_costo(self, impuesto=0, descuento=0):
+        total = self.precio
+        total += total * impuesto
+        total -= total * descuento
+        return total
 
     def mostrar_detalle(self):
-        return f"{self.nombre} - ${self.precio}"
+        return f"{self.nombre} - ${self.calcular_costo():,.0f}"
 
 
-class BarberiaPremium(Servicio):
+class AsesoriaEspecializada(Servicio):
 
     def __init__(self):
-        super().__init__("Barbería Premium", 60000)
+        super().__init__("Asesoría Especializada", 120000)
+
+    def calcular_costo(self, impuesto=0, descuento=0):
+        total = self.precio
+        total += total * impuesto
+        total -= total * descuento
+        return total
 
     def mostrar_detalle(self):
-        return f"{self.nombre} - ${self.precio}"
+        return f"{self.nombre} - ${self.calcular_costo():,.0f}"
