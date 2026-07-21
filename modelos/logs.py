6eabf9896c3d43784cec30e6aa5afc348c448ebe
sergiosierra427-bit <sergiosierra427@gmail.com>
2026-07-21 17:26:12 -0500

    Update logs.py

--- modelos/logs.py
+++ modelos/logs.py
@@ -7,7 +7,12 @@ from datetime import datetime
 
 def registrar_log(mensaje):
     """
-    Guarda un mensaje en el archivo errores.txt
+    Guarda eventos y errores en el archivo errores.txt
     """
+
     with open("errores.txt", "a", encoding="utf-8") as archivo:
-        archivo.write(f"[{datetime.now()}] {mensaje}\n")
+
+        archivo.write(
+            f"[{datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] "
+            f"{mensaje}\n"
+        )
