a1d20b8eb0090f7edf022110da8cfbe82a450f35
sergiosierra427-bit <sergiosierra427@gmail.com>
2026-07-21 16:19:55 -0500

    Update cliente.py

--- modelos/cliente.py
+++ modelos/cliente.py
@@ -1,1 +1,40 @@
-# Clase Cliente
+"""
+Clase Cliente
+"""
+
+from modelos.excepciones import ValidacionError
+
+
+class Cliente:
+
+    def __init__(self, nombre, identificacion, correo):
+        self.set_nombre(nombre)
+        self.set_identificacion(identificacion)
+        self.set_correo(correo)
+
+    def set_nombre(self, nombre):
+        if not nombre.strip():
+            raise ValidacionError("El nombre no puede estar vacío.")
+        self.__nombre = nombre
+
+    def set_identificacion(self, identificacion):
+        if not str(identificacion).isdigit():
+            raise ValidacionError("La identificación debe contener solo números.")
+        self.__identificacion = identificacion
+
+    def set_correo(self, correo):
+        if "@" not in correo:
+            raise ValidacionError("Correo electrónico inválido.")
+        self.__correo = correo
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_identificacion(self):
+        return self.__identificacion
+
+    def get_correo(self):
+        return self.__correo
+
+    def __str__(self):
+        return f"Cliente: {self.__nombre} - {self.__identificacion}"
