786210a5c5c56b6ff4aa0663f7adc2a6696ec2cc
sergiosierra427-bit <sergiosierra427@gmail.com>
2026-07-21 16:28:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -34,3 +34,34 @@ def registrar_cliente():
     except ValidacionError as e:
         registrar_log(str(e))
         print("Error:", e)
+def registrar_servicio():
+    try:
+        print("\n=== REGISTRO DE SERVICIO ===")
+
+        print("1. Corte de Cabello")
+        print("2. Manicure")
+        print("3. Barbería Premium")
+
+        opcion = input("Seleccione un servicio: ")
+
+        if opcion == "1":
+            servicio = CorteCabello()
+
+        elif opcion == "2":
+            servicio = Manicure()
+
+        elif opcion == "3":
+            servicio = BarberiaPremium()
+
+        else:
+            raise ServicioError("Servicio no válido.")
+
+        print("\nServicio registrado correctamente.")
+        print(servicio)
+
+        return servicio
+
+    except ServicioError as e:
+        registrar_log(str(e))
+        print("Error:", e)
+        return None
