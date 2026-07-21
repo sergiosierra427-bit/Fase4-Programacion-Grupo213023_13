47112b2fced62e92ffdf7e70e9dd3e44c30bf669
sergiosierra427-bit <sergiosierra427@gmail.com>
2026-07-21 16:22:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Sistema Gestión Software FJ")
+from datetime import datetime
+
+from modelos.cliente import Cliente
+from modelos.servicio import (
+    CorteCabello,
+    Manicure,
+    BarberiaPremium
+)
+from modelos.reserva import Reserva
+from modelos.logs import registrar_log
+from modelos.excepciones import (
+    ValidacionError,
+    ClienteError,
+    ServicioError,
+    ReservaError
+)
+
+clientes = []
+reservas = []
