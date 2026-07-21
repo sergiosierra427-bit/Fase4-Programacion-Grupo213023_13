c4f7ccd45788321a62d87845fb1580e3244d5645
sergiosierra427-bit <sergiosierra427@gmail.com>
2026-07-21 17:25:07 -0500

    Update reserva.py

--- modelos/reserva.py
+++ modelos/reserva.py
@@ -2,7 +2,7 @@
 Clase Reserva
 """
 
-from datetime import datetime
+from modelos.excepciones import ReservaError
 
 
 class Reserva:
@@ -11,10 +11,29 @@ class Reserva:
         self.cliente = cliente
         self.servicio = servicio
         self.fecha = fecha
+        self.estado = "Pendiente"
+
+    def confirmar(self):
+        self.estado = "Confirmada"
+
+    def cancelar(self):
+        self.estado = "Cancelada"
+
+    def procesar(self):
+
+        if self.estado == "Cancelada":
+            raise ReservaError(
+                "No se puede procesar una reserva cancelada."
+            )
+
+        self.estado = "Procesada"
 
     def mostrar_reserva(self):
+
         return (
             f"\nCliente: {self.cliente.get_nombre()}"
-            f"\nServicio: {self.servicio.mostrar_detalle()}"
+            f"\nServicio: {self.servicio.nombre}"
+            f"\nCosto: ${self.servicio.calcular_costo():,.0f}"
+            f"\nEstado: {self.estado}"
             f"\nFecha: {self.fecha.strftime('%d/%m/%Y %H:%M')}"
         )
