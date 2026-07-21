d01e1cf2437db9ded820c2bf05c45ed1bdc7228a
sergiosierra427-bit <sergiosierra427@gmail.com>
2026-07-21 16:20:31 -0500

    Update servicio.py

--- modelos/servicio.py
+++ modelos/servicio.py
@@ -1,1 +1,34 @@
-# Clase Servicio
+"""
+Clases de Servicios
+"""
+
+from abc import ABC, abstractmethod
+
+
+class Servicio(ABC):
+
+    def __init__(self, nombre, precio):
+        self.nombre = nombre
+        self.precio = precio
+
+    @abstractmethod
+    def mostrar_detalle(self):
+        pass
+
+
+class CorteCabello(Servicio):
+
+    def mostrar_detalle(self):
+        return f"Corte de Cabello - ${self.precio}"
+
+
+class Manicure(Servicio):
+
+    def mostrar_detalle(self):
+        return f"Manicure - ${self.precio}"
+
+
+class BarberiaPremium(Servicio):
+
+    def mostrar_detalle(self):
+        return f"Barbería Premium - ${self.precio}"
